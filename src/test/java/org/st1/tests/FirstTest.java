ef55a4ee3711d6b6d82550a0ef89715f5ecb7d8d
srikanth <srikanthnehrus@gmail.com>
2024-03-11 20:36:04 +0530

    Create First Testng Test

---
+++ src/test/java/org/st1/tests/FirstTest.java
@@ -0,0 +1,13 @@
+package org.st1.tests;
+
+import org.testng.annotations.Test;
+import org.st1.pages.GoogleHomePage;
+import org.st1.pages.GoogleSearchResultsPage;
+public class FirstTest {
+
+    @Test
+    public void launchgoogle(){
+        System.out.println("First Test Executed");
+
+    }
+}
